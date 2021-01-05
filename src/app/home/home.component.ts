815913d4a8bc7c45d05c719aeb1f30d76cd287dd
pankaj.agrawal@advantal.net <pankaj.agrawal@advantal.net>
2021-01-05 14:35:33 +0530

    Code pushed

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,4 +12,23 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  slides = [
+    {text: 'No other eCommerce platform allows people to start for free and grow their store as their business grows.', name: 'Rubika'},
+    {text: 'No other eCommerce platform allows people to start for free and grow their store as their.', name: 'Rubika'},
+    {text: 'No other eCommerce platform allows people to start for.', name: 'Rubika'},
+    {text: 'No other eCommerce platform allows people to start for free and grow their store as their.', name: 'Rubika'},
+    {text: 'No other eCommerce platform allows people to start for free and grow their.', name: 'Rubika'},
+    {text: 'No other eCommerce platform allows.', name: 'Rubika'}
+  ];
+
+  slideConfig = {"slidesToShow": 2, "slidesToScroll": 1, "rows": 1, "arrows": false, "dot": false};
+  
+  addSlide() {
+    this.slides.push({text: "text", name: "name"})
+  }
+
+  removeSlide() {
+    this.slides.length = this.slides.length - 1;
+  }
+
 }
